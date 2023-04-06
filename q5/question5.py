462030ca9c80dc5abe1ff61d4fe6e989b27f33f9
JooHyuckLee <dlwngur302@konkuk.ac.kr>
2023-04-06 16:15:38 +0900

    question5.py Edited

--- q5/question5.py
+++ q5/question5.py
@@ -19,11 +19,13 @@ def reverse_words(str):
 def main():
     str1 = "Two roads diverged in a yellow wood, And sorry I could not travel both And be one traveler, long I stood And looked down one as far as I could To where it bent in the undergrowth;"
     rev_str1 = reverse_words(str1)
-    print(rev_str1)
+    print("Original string was: ", str1, "\nReversed string is: ", rev_str1)
+
+    print()
     
     str2 = "Then took the other, as just as fair, And having perhaps the better claim, Because it was grassy and wanted wear; Though as for that the passing there Had worn them really about the same"
     rev_str2 = reverse_words(str2)
-    print(rev_str2)
+    print("Original string was: ", str2, "\nReversed string is: ", rev_str2)
 
     
     
